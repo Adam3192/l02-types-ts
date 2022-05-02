44682f3f6118d5e56ca45136e8d2d4c789bb36a8
Adam3192 <jeremyadixon@hotmail.com>
2022-05-02 16:35:27 -0500

    lesson 2

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+let superheroes = ["Batman", "Superman", "Wonder Woman"];
+for (let hero of superheroes) {
+    console.log(hero);
+}
+for (let hero in superheroes) {
+    console.log(hero);
+}
---
+++ index.ts
@@ -0,0 +1,29 @@
+let superheroes = ["Batman", "Superman", "Wonder Woman"];
+
+for (let hero of superheroes){
+    console.log(hero);
+}
+
+for (let hero in superheroes){
+    console.log(hero);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
